9ccfea0ce4cd5bc5a5f14106dedb0cbfac19202d
sdhhx <sdhhx@qq.com>
2018-02-27 00:28:59 +0800

    first commit

---
+++ src/main/java/cc/litstar/AppMain.java
@@ -0,0 +1,13 @@
+package cc.litstar;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
